10796bb1a0693790b4e09eec77b3cc4e8e27ddc1
Dival Banerjee <bandiv2011@gmail.com>
2019-05-21 19:17:04 -0400

    Finished the calculator
    Calculator finished

--- main.cpp
+++ main.cpp
@@ -20,14 +20,25 @@ int main(int argc, const char * argv[]) {
     double lowPowerDurationInHours;
     double cRating;
     double cellCapacityInAmpHours;
-    
+    double cellVoltage;
+    double safetyPercentage;
     //Calculated Values
     
     double highPowerCurrent;
     double lowPowerCurrent;
+    double highPowerCapacity;
+    double lowPowerCapacity;
+    double rawCapacityReq;
+    double maxSafeCurrent;
+    double totalCapacity;
+    double seriesPacks;
+    double parallelPacks;
+
 
     std::cout << "Enter motor voltage[V]: " << std::endl;
     std::cin >> motorVoltage;
+    std::cout << "Enter cell voltage[V]: " << std::endl;
+    std::cin >> cellVoltage;
     std::cout << "Enter high power level [W]: " << std::endl;
     std::cin >> highPowerInW;
     std::cout << "Enter high power duration [minutes]: " << std::endl;
@@ -38,9 +49,48 @@ int main(int argc, const char * argv[]) {
     std::cin >> lowPowerDurationInMin;
     std::cout << "Enter C Rating | C ratings above 40 C will be set to 40 C" << std::endl;
     std::cin >> cRating;
+    
+    if(cRating > 40){ //Any C Rating over 40 C is total BS. You need to measure the internal resistance of the battery. To be actually sure.
+        cRating = 40;
+        std::cout << "C rating exceeded 40. Reseting to 40 C" << std::endl;
+    }
+    
     std::cout << "Enter cell capacity [Ah]: " << std::endl;
     std::cin >> cellCapacityInAmpHours;
+    std::cout << "Enter safety percentage: " << std::endl;
+    std::cin >> safetyPercentage;
     
+    highPowerDurationInHours = highPowerDurationInMin/60;
+    lowPowerDurationInHours = lowPowerDurationInMin/60;
+    
+    highPowerCurrent = 8*(highPowerInW/motorVoltage);
+    lowPowerCurrent = 8*(lowPowerInW/motorVoltage);
+    
+    maxSafeCurrent = cellCapacityInAmpHours * cRating;
+    
+    std::cout << "Maximum safe current:" << maxSafeCurrent << std::endl;
+    
+    if(highPowerCurrent > maxSafeCurrent) {
+        std::cout << "Highpower current exceeds maximum safe current based on discharge" << std::endl;
+    }
+    
+    highPowerCapacity = highPowerCurrent * highPowerDurationInHours;
+    std::cout << "High power capacity [Ah]: " << highPowerCapacity << std::endl;
+    
+    lowPowerCapacity = lowPowerCurrent * lowPowerDurationInHours;
+    std::cout << "Low power capacity [Ah]: " << lowPowerCapacity << std::endl;
+    
+    rawCapacityReq = lowPowerCapacity + highPowerCapacity;
+    std::cout << "Combined power required [Ah]:" << rawCapacityReq << std::endl;
+    
+    totalCapacity = rawCapacityReq * ((safetyPercentage/100)+1);
+    std::cout << "Total capacity required [Ah]: " << totalCapacity << std::endl;
+    
+    seriesPacks = motorVoltage / cellVoltage;
+    parallelPacks = totalCapacity/cellCapacityInAmpHours;
+    
+    std::cout << "Number of series packs: " << seriesPacks <<std::endl;
+    std::cout << "Number of parallel packs: " << parallelPacks << std::endl;
     
     return 0;
 }
